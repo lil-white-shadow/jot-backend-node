3d1bcce1c33f774de6bf468b0b33b64fcec871a9
lil-white-shadow <kru.patel.swe@gmail.com>
2023-01-10 22:25:43 -0600

    use cors and add a test /get method

--- server/index.js
+++ server/index.js
@@ -1,8 +1,14 @@
 const express = require("express");
+const cors = require("cors");
 
 const PORT = process.env.PORT || 3001;
 
 const app = express();
+app.use(cors());
+
+app.get("/api", (req, res) => {
+  res.json({message: 'Hola from server!'});
+});
 
 app.listen(PORT, () => {
   console.log(`Server listening on ${PORT}`);
